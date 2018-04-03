a1bcab31cdebc92a0b800cd93389f7686c557f66
Jeesh Nair Murali <jeeshn@microsoft.com>
2018-04-02 23:48:49 -0700

    Little better

--- app/src/main/java/com/ubicomp/jeeshn/steptracker/MainActivity.java
+++ app/src/main/java/com/ubicomp/jeeshn/steptracker/MainActivity.java
@@ -27,9 +27,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     int readingCount = 0;
     int stepCount = 0;
+    int calculatedStepCount = 0;
     int initialCounterValue = 0;
 
     TextView txtStepCount;
+    TextView txtCalculatedStepCount;
+    int StepDetected = 1;
+    int Idle = 0;
+    int state;
+    int previousstate;
+
+    long streakStartTime;
+    long streakPrevTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,32 +46,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         txtStepCount = findViewById((R.id.txtStepCount));
+        txtCalculatedStepCount = findViewById((R.id.txtCalculatedStep));
+
         rawGraph = findViewById(R.id.graphRaw);
         smoothGraph = findViewById(R.id.graphSmooth);
 
         rawSeries = new LineGraphSeries<DataPoint>();
-        this.InitializeGraphControl(rawGraph,rawSeries);
+        this.InitializeGraphControl(rawGraph,rawSeries,20,80);
 
         smoothSeries = new LineGraphSeries<DataPoint>();
-        this.InitializeGraphControl(smoothGraph,smoothSeries);
+        this.InitializeGraphControl(smoothGraph,smoothSeries,5,80);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager
-                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mStepCounter  = mSensorManager
-                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mStepCounter  = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         StartSensor();
     }
 
-    void InitializeGraphControl(GraphView graph, LineGraphSeries<DataPoint> series)
+    void InitializeGraphControl(
+            GraphView graph, LineGraphSeries<DataPoint> series, double maxY, double maxX)
     {
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(3);
         graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setMinY(0);
-        graph.getViewport().setMaxY(20);
+        graph.getViewport().setMaxY(maxY);
 
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(80);
+        graph.getViewport().setMaxX(maxX);
 
         graph.getViewport().setScalable(true);
         graph.getViewport().setScalableY(true);
@@ -78,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mSensorManager.registerListener(
                 this,
                 mStepCounter,
-                SensorManager.SENSOR_DELAY_NORMAL);
+                SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -88,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 this.CalculateMovement(event);
                 break;
             case Sensor.TYPE_STEP_COUNTER:
-                //this.IncrementStepCount(event);
+                this.IncrementStepCount(event);
                 break;
         }
     }
@@ -136,6 +148,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         z = event.values[2] - gravity[2];
 
         double smoothd = Math.sqrt(x * x + y * y + z * z);
+
+        if(smoothd>=1.5)
+        {
+            this.state = this.StepDetected;
+            if(this.previousstate!=this.state)
+            {
+                streakStartTime = System.currentTimeMillis();
+                if ((streakStartTime - streakPrevTime) <= 250f) {
+                    streakPrevTime = System.currentTimeMillis();
+                    return;
+                }
+                streakPrevTime = streakStartTime;
+                calculatedStepCount = calculatedStepCount+1;
+                txtCalculatedStepCount.setText((String.valueOf(calculatedStepCount)));
+            }
+        }
+        else
+        {
+            this.state = this.Idle;
+            this.previousstate = this.state;
+        }
 
         this.PlotGraph(smoothd,readingCount, smoothSeries);
     }
